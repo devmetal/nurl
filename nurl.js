93fcee9000d52b206f36b738c22b1f1df773317e
unknown <metal@CZC018BC4D.nsn-intra.net>
2014-12-10 16:08:54 +0100

    updates

--- nurl.js
+++ nurl.js
@@ -1,33 +1,21 @@
-var request = require('request');
-var Q       = require('Q');
+var _request   = require('request');
+var Q          = require('Q');
+var stream     = require('stream');
+var utils      = require('utils'); 
+var JsonStream = require('JSONStream');
 
-/**
- * Nurl Logger Stream
- * It is a transformer stream
- * It will write log datas to the destination.
- * 
- * @param source
- * It is a readable
- *
- * @param dest
- * It is a writable
- * 
- */
-function NurlLogger(source,dest) {
+function NurlStream() {
 
 }
 
-/**
- * NurlJson
- * Stream reader class
- */
-function NurlJson() {
-
+function Nurl(url, options) {
+	this.url = url;
+	this.options = options || {};
+	this.options['url'] = url;
 }
 
-function makePromiseRequest(url,options) {
-	options = options || {};
-	options['url'] = url;
+Nurl.prototype.request = function() {
+	var self = this;
 
 	var def = Q.defer();
 
@@ -39,31 +27,7 @@ function makePromiseRequest(url,options) {
 		}
 	}
 
-	request(options,cb);
+	_request(options,cb);
 
 	return def.promise;
-}
-
-function makeStreamRequest(url,options,writable) {
-
-}
-
-function Nurl(url, options) {
-	this.url = url;
-	this.options = options;
-}
-
-Nurl.prototype.request = function() {
-	var self = this;
-}
-
-/**
- * Create a request stream with request package from npm
- * 
- * @param writable 
- * This is a Stream.Writable class. If we provide this
- * param, the request will be piped to this stream.
- */
-Nurl.prototype.requestStream = function(writable) {
-	var self = this;
 }
