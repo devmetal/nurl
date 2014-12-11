229dd54242191e1085dbae991db7a3fc4373416a
unknown <metal@CZC018BC4D.nsn-intra.net>
2014-12-11 15:13:49 +0100

    Updates
    modified:   nurl.js

--- nurl.js
+++ nurl.js
@@ -4,7 +4,31 @@ var stream     = require('stream');
 var utils      = require('utils'); 
 var JsonStream = require('JSONStream');
 
+utils.inherits(NurlStream, stream.Transform);
+utils.inherits(NurlHistory,stream.Duplex);
+
 function NurlStream() {
+	stream.Transform.call(this);
+
+	this._meta    = null;
+	this._request = null;
+	this._headers = null;
+	this._body    = null;
+}
+
+NurlStream.prototype._transform = function() {
+
+}
+
+function NurlHistory() {
+	stream.Duplex.call(this);
+}
+
+NurlHistory.prototype._read = function() {
+
+}
+
+NurlHistory.prototype._write = function() {
 
 }
 
@@ -12,6 +36,8 @@ function Nurl(url, options) {
 	this.url = url;
 	this.options = options || {};
 	this.options['url'] = url;
+
+	this.stream = new NurlStream();
 }
 
 Nurl.prototype.request = function() {
@@ -27,7 +53,21 @@ Nurl.prototype.request = function() {
 		}
 	}
 
-	_request(options,cb);
+	_request(options,cb)
+		.pipe(self.stream);
 
 	return def.promise;
+}
+
+var _history = new NurlHistory();
+
+module.exports = {
+	nurl:function(url,options) {
+		var nurl = new Nurl(url,options);
+		_history.write(nurl);
+		return nurl.request();
+	},
+	history:function() {
+		return _history; //TODO: Make a readable strem from _history
+	}
 }
